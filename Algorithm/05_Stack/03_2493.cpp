25e39e1be399ca35aba13b31ef6d570689b9fce1
Devdha <donghun.dev@gmail.com>
2021-12-19 21:51:35 +0900

    1

--- Algorithm/05_Stack/03_2493.cpp
+++ Algorithm/05_Stack/03_2493.cpp
@@ -1,28 +1,57 @@
-// #include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-#include <stack>
+#include <bits/stdc++.h>
+using namespace std;
 
-int n, cur;
-stack<int> s;
-stack<int> t;
+int N, tower;
+stack<pair<int, int> > s;
 
-int main() {
+int main() { 
 	ios::sync_with_stdio(0); cin.tie(0);
-	cin >> n;
-	vector<int> v(n);
-	fill(v, v + n, 0);
-	for (int i = 0; i < n; i++) {
-		cin >> c;
-		s.push(c);
-	}
-	for (int i = n; n > 0; n--) {
-		t = s;
-		cur = t.top();
-		for (int j = i; i > 0; i--) {
-			t.pop()
-			if (cur <= t.top())
-				v[i - 1] = j;
+	cin >> N;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++) {
+		cin >> tower;
+		while (!(s.empty())) {
+			if (tower < s.top().second) {
+				cout << s.top().first << " ";
+				break;
+			}
+			s.pop();
 		}
+		if (s.empty()) {
+			cout << 0 << " ";
+		}
+		s.push(make_pair(i + 1, tower));
 	}
 }
+
+
+
+
+// int n, c, cur;
+// stack<int> s;
+// stack<int> t;
+
+// int main() {
+// 	ios::sync_with_stdio(0); cin.tie(0);
+// 	cin >> n;
+// 	vector<int> v(n);
+// 	fill(v.begin(), v.begin() + n, 0);
+// 	for (int i = 0; i < n; i++) {
+// 		cin >> c;
+// 		s.push(c);
+// 	}
+// 	for (int i = n; i > 0; i--) {
+// 		t = s;
+// 		cur = t.top();
+// 		for (int j = i - 1; j > 0; j--) {
+// 			t.pop();
+// 			if (cur <= t.top()) {
+// 				v[i - 1] = j;
+// 				break;
+// 			}
+// 		}
+// 		s.pop();
+// 	}
+// 	for (int i = 0; i < n; i++) 
+// 		cout << v[i] << " ";
+// }
